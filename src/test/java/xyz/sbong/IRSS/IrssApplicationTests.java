8b720945e863ab053a5620a43059f17acb4118b6
Steve Obongo <steveobongo0@gmail.com>
2026-08-14 12:33:58 +0300

    Initial commit

---
+++ src/test/java/xyz/sbong/IRSS/IrssApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sbong.IRSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
