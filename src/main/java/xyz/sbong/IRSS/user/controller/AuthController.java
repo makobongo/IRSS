3c9d852f841882df225268a89b29cf5393462ffe
Steve Obongo <steveobongo0@gmail.com>
2026-08-15 19:27:21 +0300

    update; adjusted api for user registration to accept input

--- src/main/java/xyz/sbong/IRSS/user/controller/AuthController.java
+++ src/main/java/xyz/sbong/IRSS/user/controller/AuthController.java
@@ -1,15 +1,22 @@
 package xyz.sbong.IRSS.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.sbong.IRSS.user.entity.User;
+import xyz.sbong.IRSS.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class AuthController {
+    @Autowired
+    private UserService userService;
     @PostMapping("/register")
-    public String register() {
-        return "Registration successful";
+    public String register(@RequestBody User user) {
+        userService.registerUser(user);
+        return "User registered successfully";
     }
 
     @PostMapping("/login")
