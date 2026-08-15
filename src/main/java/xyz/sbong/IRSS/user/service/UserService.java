15c7a366632bccbd8f2cfaa93414068253138ba6
Steve Obongo <steveobongo0@gmail.com>
2026-08-15 20:57:42 +0300

    update: added a service method to fetch all users

--- src/main/java/xyz/sbong/IRSS/user/service/UserService.java
+++ src/main/java/xyz/sbong/IRSS/user/service/UserService.java
@@ -2,8 +2,11 @@ package xyz.sbong.IRSS.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xyz.sbong.IRSS.user.dto.UserDto;
 import xyz.sbong.IRSS.user.entity.User;
 import xyz.sbong.IRSS.user.respository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -11,5 +14,9 @@ public class UserService {
     private UserRepository userRepository;
     public void registerUser(User user) {
         userRepository.save(user);
+    }
+
+    public List<UserDto> getAllUsers() {
+        return userRepository.fetchAllUsers();
     }
 }
