9ad7d233260ca41026afee61274fa2a6b1295b86
Steve Obongo <steveobongo0@gmail.com>
2026-08-15 19:28:12 +0300

    update: added fields

--- src/main/java/xyz/sbong/IRSS/user/entity/User.java
+++ src/main/java/xyz/sbong/IRSS/user/entity/User.java
@@ -1,4 +1,28 @@
 package xyz.sbong.IRSS.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String role;
+    private String department;
+    private Boolean isActive;
 }
