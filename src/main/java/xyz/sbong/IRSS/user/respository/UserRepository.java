2bc34823e7011edec4159a07ddf779b95b332007
Steve Obongo <steveobongo0@gmail.com>
2026-08-15 20:56:48 +0300

    update: added query to fetch specific fields

--- src/main/java/xyz/sbong/IRSS/user/respository/UserRepository.java
+++ src/main/java/xyz/sbong/IRSS/user/respository/UserRepository.java
@@ -1,9 +1,14 @@
 package xyz.sbong.IRSS.user.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import xyz.sbong.IRSS.user.dto.UserDto;
 import xyz.sbong.IRSS.user.entity.User;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Query("SELECT new xyz.sbong.IRSS.user.dto.UserDto(u.id, u.firstName, u.lastName, u.email, u.phoneNumber, u.department, u.role, u.isActive) FROM User u")
+    List<UserDto> fetchAllUsers();
 }
