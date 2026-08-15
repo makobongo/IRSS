7b38360691d00003b63433856f6fa4326c1715bd
Steve Obongo <steveobongo0@gmail.com>
2026-08-15 21:16:00 +0300

    update: user register method to hash password

--- src/main/java/xyz/sbong/IRSS/user/service/UserService.java
+++ src/main/java/xyz/sbong/IRSS/user/service/UserService.java
@@ -1,6 +1,7 @@
 package xyz.sbong.IRSS.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import xyz.sbong.IRSS.user.dto.UserDto;
 import xyz.sbong.IRSS.user.entity.User;
@@ -12,7 +13,10 @@ import java.util.List;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public void registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
