52ecb40b0db6a3cf2780d61e8e0bcbc69a298e8b
Steve Obongo <steveobongo0@gmail.com>
2026-08-16 14:10:51 +0300

    update: adjusted verifyuser method

--- src/main/java/xyz/sbong/IRSS/user/service/AuthService.java
+++ src/main/java/xyz/sbong/IRSS/user/service/AuthService.java
@@ -1,6 +1,9 @@
 package xyz.sbong.IRSS.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import xyz.sbong.IRSS.user.entity.User;
@@ -12,12 +15,15 @@ public class AuthService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     public void registerUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     public String verifyUser(User user) {
-        return "success";
+        return user.getEmail() + "success!";
     }
 }
