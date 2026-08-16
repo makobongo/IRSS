cab8e7af8da0146312a5acf31fe821eba1cde97a
Steve Obongo <steveobongo0@gmail.com>
2026-08-16 14:15:06 +0300

    fix: removed authentication manager

--- src/main/java/xyz/sbong/IRSS/user/service/AuthService.java
+++ src/main/java/xyz/sbong/IRSS/user/service/AuthService.java
@@ -1,9 +1,6 @@
 package xyz.sbong.IRSS.user.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import xyz.sbong.IRSS.user.entity.User;
@@ -15,8 +12,6 @@ public class AuthService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     public void registerUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
