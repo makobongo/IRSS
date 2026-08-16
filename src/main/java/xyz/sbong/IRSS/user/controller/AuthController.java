f395e22c9e3bfbd9aa3ffcbd7aea9a89b9747aa0
Steve Obongo <steveobongo0@gmail.com>
2026-08-16 14:10:06 +0300

    update: adjusted api for login to get request body

--- src/main/java/xyz/sbong/IRSS/user/controller/AuthController.java
+++ src/main/java/xyz/sbong/IRSS/user/controller/AuthController.java
@@ -6,21 +6,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.sbong.IRSS.user.entity.User;
-import xyz.sbong.IRSS.user.service.UserService;
+import xyz.sbong.IRSS.user.service.AuthService;
 
 @RestController
 @RequestMapping("/api/user")
 public class AuthController {
     @Autowired
-    private UserService userService;
+    private AuthService authService;
     @PostMapping("/register")
     public String register(@RequestBody User user) {
-        userService.registerUser(user);
+        authService.registerUser(user);
         return "User registered successfully";
     }
 
     @PostMapping("/login")
-    public String login() {
-        return "Login successful";
+    public String login(@RequestBody User user) {
+        return authService.verifyUser(user);
     }
 }
