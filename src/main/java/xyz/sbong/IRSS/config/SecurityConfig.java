fb9746a13d5948275858aeb870dabc9a70a80336
Steve Obongo <steveobongo0@gmail.com>
2026-08-30 14:27:07 +0300

    update: added filter chain to filter incoming requests

--- src/main/java/xyz/sbong/IRSS/config/SecurityConfig.java
+++ src/main/java/xyz/sbong/IRSS/config/SecurityConfig.java
@@ -2,29 +2,29 @@ package xyz.sbong.IRSS.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return  new BCryptPasswordEncoder();
-    }
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request-> request
-                        .requestMatchers("/api/user/register", "/api/user/login", "/api/users/all").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(customizer->customizer.disable())
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults()) //form login and logout for the browser
+                .httpBasic(Customizer.withDefaults()) //form login for the http client
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 }
